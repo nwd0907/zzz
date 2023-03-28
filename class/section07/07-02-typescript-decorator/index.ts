c8d646482b1c44fd449e36bfdc4d3672a468468e
nwd0907 <nwd0907@newbizstart.com>
2023-03-28 18:24:20 +0900

    07-02-typescript-decorator

---
+++ class/section07/07-02-typescript-decorator/index.ts
@@ -0,0 +1,14 @@
+// const qqq: string = "철수"
+
+// console.log(qqq)
+
+function Controller(aaaaaaaaaaaaa: any){
+    console.log("===============")
+    console.log(aaaaaaaaaaaaa)
+    console.log("===============")
+}
+
+@Controller
+class CatsController {
+
+}
