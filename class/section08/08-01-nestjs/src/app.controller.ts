84b2f3cb816f3a9c63a68f21e66e71101e4e2463
nwd0907 <nwd0907@newbizstart.com>
2023-03-29 16:19:53 +0900

    08-01-nestjs

--- class/section08/08-01-nestjs/src/app.controller.ts
+++ class/section08/08-01-nestjs/src/app.controller.ts
@@ -5,15 +5,23 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/products/buy')
   getHello(): string {
     const qqq = 3;
 
     const profile = {
       age: 13,
-      school: '다람쥐초등학교',
+      school: '다람쥐초등학교', // trailingComma
     };
 
-    return this.appService.getHello();
+    // 세미콜론의 중요성
+    // let aaa
+    // aaa = ["철수", "영희", "훈이"]
+
+    // [1, 2, 3].map((el) => {
+    //   console.log(el)
+    // })
+
+    return this.appService.qqq();
   }
 }
--- class/section08/08-01-nestjs/src/app.module.ts
+++ class/section08/08-01-nestjs/src/app.module.ts
@@ -5,6 +5,6 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService], // const appService = new AppService
 })
 export class AppModule {}
