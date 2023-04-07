faa33a7a06c0eb2898ca1f8a2a03563f74405fc3
nwd0907 <nwd0907@newbizstart.com>
2023-04-07 12:26:35 +0900

    10-06-typeorm-crud-one-to-one

---
+++ class/section10/10-06-typeorm-crud-one-to-one/src/apis/products/products.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductSaleslocation } from '../productsSaleslocations/entities/productSaleslocation.entity';
+import { ProductsSaleslocationsService } from '../productsSaleslocations/productsSaleslocations.service';
+import { Product } from './entities/product.entity';
+import { ProductsResolver } from './products.resolver';
+import { ProductsService } from './products.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Product, //
+      ProductSaleslocation, // 좋은 방법 아님 => 나중에 모듈로 합쳐서 한번에 가져오자!!
+    ]),
+  ],
+
+  providers: [
+    ProductsResolver, //
+    ProductsService,
+    ProductsSaleslocationsService, // 좋은 방법 아님 => 나중에 모듈로 합쳐서 한번에 가져오자!!
+  ],
+})
+export class ProductsModule {}
