a4d72e44141db7f09da40a7ab63b0c0bdb6c1e6c
nwd0907 <nwd0907@newbizstart.com>
2023-04-12 16:50:43 +0900

    11-04-login-refresh-cookie

---
+++ class/section11/11-04-login-refresh-cookie/src/apis/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
+import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
+import { JwtAccessStrategy } from './strategies/jwt-access.strategy';
+
+@Module({
+  imports: [
+    JwtModule.register({}), // 설정도 줄 수 있음
+    UsersModule, //
+  ],
+
+  providers: [
+    JwtAccessStrategy,
+    AuthResolver, //
+    AuthService,
+  ],
+})
+export class AuthModule {}
