25244da8b33c832022ba13f24c5210754fcbee5a
nwd0907 <nwd0907@newbizstart.com>
2023-04-24 18:50:46 +0900

    15-03-msa-nginx

---
+++ class/section15/15-03-msa-nginx/services/auth/src/app.resolver.ts
@@ -0,0 +1,19 @@
+// import { Controller, Get } from '@nestjs/common';
+// import { AppService } from './app.service';
+
+import { Mutation, Query, Resolver } from '@nestjs/graphql';
+
+@Resolver()
+export class AppResolver {
+  // constructor(private readonly appService) {}
+
+  @Mutation(() => String)
+  login() {
+    return 'accessToken!!!';
+  }
+
+  @Query(() => String)
+  aaa() {
+    return 'aaa';
+  }
+}
